0fdf64748cc7073ffb9be658aa5b7966ab80226e
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-10 18:21:56 +0000

    Styling images on the page  🐒

--- src/Components/Rows.js
+++ src/Components/Rows.js
@@ -21,7 +21,7 @@ import './rows.css';
     <h2>{title}</h2>
     <div className={'row_posters'}>
     {movies.map(movie=>(
-        <img className={`row_poster ${isLargeRow && "rowPoster_large"}`} key={movie.id} src = {`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt = {movie.name} /> 
+        <img className={`row_poster ${isLargeRow && 'rowPoster_large'}`} key={movie.id} src = {`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt = {movie.name} /> 
     ))}
     </div>
     </div>
