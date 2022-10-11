a86372dbf26c3771d5be6df26af05f1382623760
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-11 16:50:45 +0000

    Banner styling 🐵

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,8 +1,8 @@
 import axios from '../axios';
 import React, { useEffect, useState } from 'react'
 import requests from '../request';
+import './Banner.css';
 const base_url = "https://image.tmdb.org/t/p/original/";
-
 function Banner() {
     const [movies,setMovies] = useState([]);
     useEffect(()=>{
@@ -10,32 +10,31 @@ function Banner() {
             const request = await axios.get(requests.fetchNetflixOriginals);
             setMovies(request.data.results[
                 Math.floor(Math.random() * request.data.results.length - 1)
-            ]);
+            ])
+            return request;
         }
         fetchData();
     },[])
   return (
-    <header className='Banner' 
-    style={{backgroundSize: "cover",
-            backgroundImage: `url("${base_url}${movies?.backdrop_path}")`,
-           backgroundPosition : "center center",
-    }}
-    >
-     <div className='Banner_content'>
-         {/* {Banner header} */}
-      <h1>{
-         movies?.title || movies?.name || movies.original_name
-      }</h1>
-       <div className='banner_button'>
-          <button className='banner_button'>Play</button>
-          <button className='banner_button'>My List</button>
-       </div>
-      {/* {2 buttons} */}
-      <h3 className='header_description'>{movies?.overview}</h3>
-      {/* description */}
-     </div>
-     
-    </header>
+<header className='banner'
+ style = {{
+    backgroundPosition: "center center",
+    backgroundImage : `url("${base_url}${movies.backdrop_path}")`,
+    backgroundSize: "cover",
+ }}
+>
+    <div className='banner_content'>
+      {/* banner header */}
+        <h1 className='banner_header'>{movies?.name||movies?.title||movies.original_name}</h1>
+    {/* two buttons */}
+    <button className='banner_buttons'>Play</button>
+    <button className='banner_buttons'>My List</button>
+    <h3 className='banner_description'>{movies?.overview}</h3>
+    </div>
+
+    
+
+</header>
   )
 }
 
