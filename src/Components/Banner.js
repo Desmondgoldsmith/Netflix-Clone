6d890c9721c06c88952a72871b66a79137f9c684
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-11 15:28:13 +0000

    Banner init 🐰

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react'
+import axios from '../axios';
+import React, { useEffect, useState } from 'react'
+import requests from '../request';
 
 function Banner() {
     const [movies,setMovies] = useState([]);
+    useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovies(request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+            ]);
+        }
+        fetchData();
+    })
   return (
     <header className='Banner_header'>
       {/* {Banner header} */}
